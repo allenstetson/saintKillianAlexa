2f8e338323df2c66885d2c2e354b2f34dcca723e
lemonsharktooth <wiles.james@gmail.com>
2019-07-17 00:03:27 -0700

    Working reminder (1 minute)

--- lambda/us-east-1_stKillian/app.py
+++ lambda/us-east-1_stKillian/app.py
@@ -8,19 +8,26 @@
 # Imports
 # =============================================================================
 # Standard imports
+import datetime
 import logging
+import pytz
 import random
 
 # ASK imports
 from ask_sdk.standard import StandardSkillBuilder
 from ask_sdk_core.dispatch_components import (AbstractExceptionHandler,
-        AbstractRequestHandler, AbstractRequestInterceptor,
-        AbstractResponseInterceptor)
+    AbstractRequestHandler, AbstractRequestInterceptor,
+    AbstractResponseInterceptor)
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_model import (Response, IntentRequest, DialogState,
-        SlotConfirmationStatus, Slot)
+    SlotConfirmationStatus, Slot)
+from ask_sdk_model.services import ServiceException
+from ask_sdk_model.services.reminder_management import (
+    Trigger, TriggerType, AlertInfo, SpokenInfo, SpokenText, PushNotification,
+    PushNotificationStatus, ReminderRequest)
 from ask_sdk_model.slu.entityresolution import StatusCode
-from ask_sdk_model.ui import SimpleCard, StandardCard
+from ask_sdk_model.ui import (
+    AskForPermissionsConsentCard, SimpleCard, StandardCard)
 
 # Local imports
 import killian_data
@@ -70,35 +77,6 @@ class LaunchRequestHandler(AbstractRequestHandler):
 # =============================================================================
 # Handlers
 # =============================================================================
-class ParishPhoneHandler(AbstractRequestHandler):
-    """Object handling all initial requests."""
-    def can_handle(self, handler_input):
-        """Inform the request handler of what intents can be handled."""
-        return is_intent_name("ParishPhoneIntent")(handler_input)
-
-    def handle(self, handler_input):
-        """Handle the launch request; fetch and serve appropriate response.
-        
-        Args:
-            handler_input (ask_sdk_core.handler_input.HandlerInput):
-                Input from Alexa.
-
-        Raises:
-            ValueError:
-                If something other than the sanctioned app calls this intent.
-
-        Returns:
-            ask_sdk_model.response.Response
-                Response for this intent and device.
-        
-        """
-        speech, reprompt, cardTitle, cardText, cardImage = getParishPhoneResponse()
-        handler_input.response_builder.speak(speech).ask(reprompt).set_card(
-                StandardCard(title=cardTitle, text=cardText, image=cardImage)
-        ).set_should_end_session(False)
-        return handler_input.response_builder.response
-
-
 class MassTimeHandler(AbstractRequestHandler):
     """Object handling all initial requests."""
     def can_handle(self, handler_input):
@@ -161,6 +139,92 @@ class NextMassHandler(AbstractRequestHandler):
             StandardCard(title=cardTitle, text=cardText, image=cardImage)
         ).set_should_end_session(False)
         return handler_input.response_builder.response
+
+class NotifyNextMassHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("NotifyNextMassIntent")(handler_input)
+
+    def handle(self, handler_input):
+        logging.info("Running NotifyNextMassHandler")
+        responseBuilder = handler_input.response_builder
+        requestEnvelope = handler_input.request_envelope
+        permissions = requestEnvelope.context.system.user.permissions
+        serviceFactory = handler_input.service_client_factory
+        reminderService = serviceFactory.get_reminder_management_service()
+
+        # If no permissions
+        if not (permissions and permissions.consent_token):
+            logging.info("user hasn't granted reminder permission")
+            speech = "Please give Saint Killian permissions to set reminders"
+            speech += " using your Alexa app."
+            permissions = ["alexa::alerts:reminders:skill:readwrite"]
+            card = AskForPermissionsConsentCard(permissions=permissions)
+            return responseBuilder.speak(speech).set_card(card).response
+
+        # Else, let's set up the permission
+        logging.info("Necessary permissions found. Creating reminder.")
+        now = datetime.datetime.now(pytz.timezone("America/Los_Angeles"))
+        reminderTime = now + datetime.timedelta(minutes=+1)
+        reminderString = reminderTime.strftime("%Y-%m-%dT%H:%M:%S")
+        trigger = Trigger(TriggerType.SCHEDULED_ABSOLUTE, reminderString,
+                          time_zone_id="America/Los_Angeles")
+        msg = "It is time to leave for mass."
+        markup = "<speak>{}</speak>".format(msg)
+        reminderSpeech = SpokenText(locale="en-US", ssml=markup, text=msg)
+        alertInfo = AlertInfo(SpokenInfo([reminderSpeech]))
+        pushNotification = PushNotification(PushNotificationStatus.ENABLED)
+        reminderRequest = ReminderRequest(reminderTime, trigger, alertInfo,
+                                          pushNotification)
+        try:
+            response = reminderService.create_reminder(reminderRequest)
+        except ServiceException as e:
+            logger.error(e)
+            raise e
+
+        speech = "I will remind you at {}:{}{} to leave for mass."
+        hour = reminderTime.hour
+        minute = "{:02d}".format(reminderTime.minute)
+        if hour >= 12:
+            suffix = "pm"
+        else:
+            suffix = "am"
+
+        if hour > 12:
+            hour = hour - 12
+        speech = speech.format(hour, minute, suffix)
+        card = SimpleCard("St. Killian", "Reminder set for Mass.")
+        return responseBuilder.speak(speech).set_card(card) \
+            .set_should_end_session(True).response
+
+
+class ParishPhoneHandler(AbstractRequestHandler):
+    """Object handling all initial requests."""
+    def can_handle(self, handler_input):
+        """Inform the request handler of what intents can be handled."""
+        return is_intent_name("ParishPhoneIntent")(handler_input)
+
+    def handle(self, handler_input):
+        """Handle the launch request; fetch and serve appropriate response.
+        
+        Args:
+            handler_input (ask_sdk_core.handler_input.HandlerInput):
+                Input from Alexa.
+
+        Raises:
+            ValueError:
+                If something other than the sanctioned app calls this intent.
+
+        Returns:
+            ask_sdk_model.response.Response
+                Response for this intent and device.
+        
+        """
+        speech, reprompt, cardTitle, cardText, cardImage = getParishPhoneResponse()
+        handler_input.response_builder.speak(speech).ask(reprompt).set_card(
+                StandardCard(title=cardTitle, text=cardText, image=cardImage)
+        ).set_should_end_session(False)
+        return handler_input.response_builder.response
+
 
 # =============================================================================
 # Fallback Handler
@@ -269,6 +333,7 @@ sb.add_request_handler(FallbackIntentHandler())
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(MassTimeHandler())
 sb.add_request_handler(NextMassHandler())
+sb.add_request_handler(NotifyNextMassHandler())
 sb.add_request_handler(ParishPhoneHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
 
